c70e81e8068cc8586bdbcabcc7f87c618c6da315
megaconfidence <cokoghenun@gmail.com>
2025-03-28 20:28:06 +0000

    chore: File renames

--- src/transformer.ts
+++ src/transformer.ts
@@ -8,36 +8,35 @@ export class Transformer extends WorkflowEntrypoint<Env, Payload> {
 		const fileType = event.payload.fileType;
 		const { resize, format, filter } = event.payload.options;
 
-		let file: any = await (await this.env.R2.get(id))?.arrayBuffer();
-
 		if (resize) {
 			await step.do('resize image', async () => {
-				const image = await Jimp.fromBuffer(file);
+				const file = await this.env.R2.get(id);
+				const image = await Jimp.fromBuffer(await file!.arrayBuffer());
 				image.resize(resize);
-				file = await image.getBuffer(fileType);
+				await this.env.R2.put(id, await image.getBuffer(fileType));
 			});
 		}
 
 		if (filter) {
 			await step.do('filter image', async () => {
-				const image = await Jimp.fromBuffer(file);
+				const file = await this.env.R2.get(id);
+				const image = await Jimp.fromBuffer(await file!.arrayBuffer());
 				const fOptions = Object.keys(filter).map((n) => {
 					const fValue = (filter as any)[n];
 					return { apply: n, params: fValue ? [fValue] : undefined };
 				}) as any;
 				image.color(fOptions);
-				file = await image.getBuffer(fileType);
+				await this.env.R2.put(id, await image.getBuffer(fileType));
 			});
 		}
 
 		if (format) {
 			await step.do('format image', async () => {
-				const image = await Jimp.fromBuffer(file);
+				const file = await this.env.R2.get(id);
+				const image = await Jimp.fromBuffer(await file!.arrayBuffer());
 				const mimes = { png: 'image/png', bmp: 'image/bmp', gif: 'image/gif', jpeg: 'image/jpeg' } as const;
-				file = await image.getBuffer(fileType);
+				await this.env.R2.put(id, await image.getBuffer(mimes[format]));
 			});
 		}
-
-		await this.env.R2.put(id, file);
 	}
 }
